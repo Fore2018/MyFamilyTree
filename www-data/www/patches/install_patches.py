2d903431ba5c57fc076415f37497d7a97da9836c
zxxml <zxxml@outlook.com>
2018-10-11 17:42:17 +0800

    开始制作插件

--- www-data/www/patches/install_patches.py
+++ www-data/www/patches/install_patches.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
 import shutil
 
 
 class PatchesInstaller:
     def __init__(self):
         with open('patches.json', 'r', encoding='utf-8') as fp:
-            self.patches = json.load(fp)['patches']
+            content = json.load(fp)
+        self.files = content['files']
+        self.dirs = content['dirs']
 
     def install(self):
-        for patch in self.patches:
+        for patch in self.files:
             print(patch['description'])
-            shutil.copyfile(patch['to'], patch['backup'])
+            if 'backup' in patch and not os.path.exists(patch['backup']):
+                if not os.path.exists(os.path.dirname(patch['backup'])):
+                    os.makedirs(os.path.dirname(patch['backup']))
+                shutil.copyfile(patch['to'], patch['backup'])
             shutil.copyfile(patch['from'], patch['to'])
+        for patch in self.dirs:
+            print(patch['description'])
+            if 'backup' in patch and not os.path.exists(patch['backup']):
+                shutil.copytree(patch['to'], patch['backup'])
+            shutil.rmtree(patch['to'])
+            shutil.copytree(patch['from'], patch['to'])
 
 
 if __name__ == '__main__':
---
+++ www-data/www/patches/quelaag_furysword/embed.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined('puyuetian')) {
+    exit('403');
+}
+
+// 去除标题连字符
+$_G['SET']['EMBED_HEADMARKS'] .= <<<EOF
+<script>
+    document.title = document.title.replace(/\s-$/, "");
+</script>
+EOF;
+
+// 修改网站名
+$_G['SET']['WEBNAME'] = $_G['SET']['app_克拉格_克拉格的魔剑_webname'];
+
+// 添加切换版式选项
+$_G['SET']['FOOTERHTMLCODE'] .= <<<EOF
+<a href="https://www.linkfire.org/">电脑版</a>
+<a href="https://m.linkfire.org/">手机版</a>
+<a target="_blank" href="mailto:admin@linkfire.org">联系站长</a>
+EOF;
+
+// 修改一些样式
+$_G['SET']['EMBED_HEAD'] .= <<<EOF
+<style type="text/css">
+.readcontent {text-align: justify;}
+p {text-align: justify;}
+pre {overflow: auto; white-space: pre !important; width: 100% !important; word-wrap: break-word;}
+.fly-footer a{padding:0 6px; font-weight: 400; color: #737573;}
+</style>
+EOF;
